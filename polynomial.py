53767cf55f495ea3cac1a25f8cc2b88c09861c22
iamcalledayush <ayushsharmaagain@gmail.com>
2024-01-27 18:17:44 -0500

    cs 506 exercise part b

--- polynomial.py
+++ polynomial.py
@@ -20,19 +20,54 @@ class Add:
     def __repr__(self):
         return repr(self.p1) + " + " + repr(self.p2)
 
+# class Mul:
+#     def __init__(self, p1, p2):
+#         self.p1 = p1
+#         self.p2 = p2
+
+#     def __repr__(self):
+#         if isinstance(self.p1, Add):
+#             if isinstance(self.p2, Add):
+#                  return "( " + repr(self.p1) + " ) * ( " + repr(self.p2) + " )"
+#             return "( " + repr(self.p1) + " ) * " + repr(self.p2)
+#         if isinstance(self.p2, Add):
+#             return repr(self.p1) + " * ( " + repr(self.p2) + " )"
+#         return repr(self.p1) + " * " + repr(self.p2)
+
 class Mul:
     def __init__(self, p1, p2):
         self.p1 = p1
         self.p2 = p2
 
     def __repr__(self):
-        if isinstance(self.p1, Add):
-            if isinstance(self.p2, Add):
-                 return "( " + repr(self.p1) + " ) * ( " + repr(self.p2) + " )"
-            return "( " + repr(self.p1) + " ) * " + repr(self.p2)
-        if isinstance(self.p2, Add):
-            return repr(self.p1) + " * ( " + repr(self.p2) + " )"
-        return repr(self.p1) + " * " + repr(self.p2)
+        def needs_parentheses(term):
+            return isinstance(term, (Add, Sub, Div))
+
+        left = f"( {repr(self.p1)} )" if needs_parentheses(self.p1) else repr(self.p1)
+        right = f"( {repr(self.p2)} )" if needs_parentheses(self.p2) else repr(self.p2)
+        return f"{left} * {right}"
+
+
+class Div:
+    def __init__(self, p1, p2):
+        self.p1 = p1
+        self.p2 = p2
+
+    def __repr__(self):
+        left = f"( {repr(self.p1)} )" if isinstance(self.p1, (Add, Sub, Mul)) else repr(self.p1)
+        right = f"( {repr(self.p2)} )" if isinstance(self.p2, (Add, Sub, Mul)) else repr(self.p2)
+        return f"{left} / {right}"
+
+class Sub:
+    def __init__(self, p1, p2):
+        self.p1 = p1
+        self.p2 = p2
+
+    def __repr__(self):
+        left = f"( {repr(self.p1)} )" if isinstance(self.p1, (Add, Sub)) else repr(self.p1)
+        right = f"( {repr(self.p2)} )" if isinstance(self.p2, (Add, Sub)) else repr(self.p2)
+        return f"{left} - {right}"
+
 
 
 poly = Add( Add( Int(4), Int(3)), Add( X(), Mul( Int(1), Add( Mul(X(), X()), Int(1)))))
