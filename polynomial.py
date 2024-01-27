f6ab05bfa87af7a4035511f6aa2153b09a32671c
iamcalledayush <ayushsharmaagain@gmail.com>
2024-01-27 18:25:02 -0500

    cs 506 exercise part c

--- polynomial.py
+++ polynomial.py
@@ -4,6 +4,9 @@ class X:
 
     def __repr__(self):
         return "X"
+    
+    def evaluate(self, value):
+        return value
 
 class Int:
     def __init__(self, i):
@@ -12,6 +15,9 @@ class Int:
     def __repr__(self):
         return str(self.i)
 
+    def evaluate(self, value):
+        return self.i
+
 class Add:
     def __init__(self, p1, p2):
         self.p1 = p1
@@ -19,6 +25,9 @@ class Add:
 
     def __repr__(self):
         return repr(self.p1) + " + " + repr(self.p2)
+
+    def evaluate(self, value):
+        return self.p1.evaluate(value) + self.p2.evaluate(value)
 
 # class Mul:
 #     def __init__(self, p1, p2):
@@ -46,6 +55,9 @@ class Mul:
         left = f"( {repr(self.p1)} )" if needs_parentheses(self.p1) else repr(self.p1)
         right = f"( {repr(self.p2)} )" if needs_parentheses(self.p2) else repr(self.p2)
         return f"{left} * {right}"
+    
+    def evaluate(self, value):
+        return self.p1.evaluate(value) * self.p2.evaluate(value)
 
 
 class Div:
@@ -57,6 +69,9 @@ class Div:
         left = f"( {repr(self.p1)} )" if isinstance(self.p1, (Add, Sub, Mul)) else repr(self.p1)
         right = f"( {repr(self.p2)} )" if isinstance(self.p2, (Add, Sub, Mul)) else repr(self.p2)
         return f"{left} / {right}"
+    
+    def evaluate(self, value):
+        return self.p1.evaluate(value) / self.p2.evaluate(value)
 
 class Sub:
     def __init__(self, p1, p2):
@@ -67,8 +82,13 @@ class Sub:
         left = f"( {repr(self.p1)} )" if isinstance(self.p1, (Add, Sub)) else repr(self.p1)
         right = f"( {repr(self.p2)} )" if isinstance(self.p2, (Add, Sub)) else repr(self.p2)
         return f"{left} - {right}"
+    
+    def evaluate(self, value):
+        return self.p1.evaluate(value) - self.p2.evaluate(value)
 
 
 
 poly = Add( Add( Int(4), Int(3)), Add( X(), Mul( Int(1), Add( Mul(X(), X()), Int(1)))))
 print(poly)
+poly = Add(Add(Int(4), Int(3)), Add(X(), Mul(Int(1), Add(Mul(X(), X()), Int(1)))))
+print(poly.evaluate(-1)) # ouptut is 8, because: 4+3+(-1)+1*(-1*-1 + 1) = 4+3-1+2 = 8
